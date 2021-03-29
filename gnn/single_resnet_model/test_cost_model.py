b29ce4afe44a5b1bb455f29a788e97c7ad3aad62
Yi Xiaodong <409070169@qq.com>
2021-03-29 23:17:13 +0800

    add rmmlm

--- gnn/single_resnet_model/test_cost_model.py
+++ gnn/single_resnet_model/test_cost_model.py
@@ -1,8 +1,8 @@
 import sys
-from cost_model import CostModel
-
 sys.path.append("../")
 sys.path.append("../../")
 sys.path.append("../../protobuf/")
+from cost_model import CostModel
+
 cost_model = CostModel()
 cost_model.test_accuracy()
